e51d174c9388f7728704a9636397aab0f06d2f7d
Benwager12 <benwager@live.co.uk>
2023-04-07 02:40:43 +0100

    Added allow list cleanup

--- cogs/commands/allow.py
+++ cogs/commands/allow.py
@@ -1,4 +1,6 @@
 import discord
+import discord.ext.commands
+from discord import Guild
 from discord.ext import commands
 from discord.ext.commands import Context
 
@@ -8,7 +10,7 @@ from helpers.string import subcommand
 
 
 class Allow(commands.Cog):
-    def __init__(self, bot):
+    def __init__(self, bot: discord.ext.commands.Bot):
         self.bot = bot
         self.help = "Allow users to use the bot."
 
@@ -40,6 +42,26 @@ class Allow(commands.Cog):
             await ctx.reply(f"Removed `{user.display_name}` from the allowed users list.")
         else:
             await ctx.reply(f"`{user.display_name}` is not on the allowed users list.")
+
+    @_allow.command()
+    async def cleanup(self, ctx: Context):
+        initial_users = allow_list.get_users()
+        removed_users = []
+        for user_id, added_by, guild_id in initial_users:
+            guild: Guild = self.bot.get_guild(guild_id)
+            member = await guild.fetch_member(user_id)
+            added_user = await guild.fetch_member(added_by)
+            if not member or not added_user and not is_owner_user(user_id):
+                allow_list.remove_user(user_id)
+                removed_users.append(user_id)
+        removed_amount = len(removed_users)
+
+        if removed_amount == 0:
+            await ctx.reply("No users were removed from the allowed users list.")
+            return
+        await ctx.reply(f"Cleaned up the allowed users list, {removed_amount} users were removed.")
+        print(f"Cleaned up the allowed users list, {removed_amount} users were removed. "
+              f"Removed users: {removed_users}.")
 
 
 async def setup(bot):
