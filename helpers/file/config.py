7b9626bbcddca117832af2bf7c0ee406077614ad
Benwager12 <benwager@live.co.uk>
2023-04-07 02:41:30 +0100

    Made config actually check if a file exists first.

--- helpers/file/config.py
+++ helpers/file/config.py
@@ -1,3 +1,5 @@
+import os
+
 from helpers.file.utility import JsonFile
 
 _config = JsonFile('config.json')
@@ -5,24 +7,38 @@ _config = JsonFile('config.json')
 
 def get(key, default=None):
     """Get a config value."""
+    if not _config.file_exists():
+        return os.getenv(key, default)
     return _config.get(key, default)
 
 
 def get_file():
-    _config.fetch_file()
-    return _config.file
+    if not _config.file_exists():
+        return None
+
+    return _config.fetch_file()
 
 
 def get_json():
+    if not _config.file_exists():
+        return None
     return _config.data
 
 
 def set(key, value):
     """Set a config value."""
+    if not _config.file_exists():
+        os.environ[key] = value
     _config.set(key, value)
 
 
 def save():
     """Save the config to disk."""
+    if not _config.file_exists():
+        return
     _config.save()
     _config.fetch_file()
+
+
+def file_exists():
+    return _config.file_exists()
